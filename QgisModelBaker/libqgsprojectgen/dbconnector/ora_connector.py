572075774aca545a246d6761f47949c36b48c518
yesidpol <yesidpol.3@gmail.com>
2019-07-25 15:20:02 -0500

    [ora] Add method to get tables information for interlis layers

--- QgisModelBaker/libqgsprojectgen/dbconnector/ora_connector.py
+++ QgisModelBaker/libqgsprojectgen/dbconnector/ora_connector.py
@@ -85,6 +85,81 @@ class OraConnector(DBConnector):
     def _metadata_exists(self):
         return self._table_exists(OraConnector.METADATA_TABLE)
 
+    def get_tables_info(self):
+        res = []
+        if self.schema:
+            ln = "\n"
+            stmt = ""
+
+            metadata_exists = self.metadata_exists()
+
+            stmt += ln + "SELECT DISTINCT "
+            stmt += ln + "      tbls.OWNER AS schemaname"
+            stmt += ln + "    , tbls.TABLE_NAME AS tablename"
+            stmt += ln + "    , Col.COLUMN_NAME AS primary_key"
+            stmt += ln + "    , clm.COLUMN_NAME AS geometry_column"
+            if metadata_exists:
+                stmt += ln + "    , tsrid.SETTING as srid"
+                stmt += ln + "    , p.SETTING AS kind_settings"
+                stmt += ln + "    , alias.SETTING AS table_alias"
+                stmt += ln + "    , SUBSTR(c.ILINAME, 0,INSTR(ILINAME,'.')-1) AS model"
+                stmt += ln + "    , c.ILINAME AS ili_name"
+                stmt += ln + "    , ( SELECT LISTAGG(SETTING, ',') WITHIN GROUP (ORDER BY CASE"
+                stmt += ln + "            WHEN cprop.tag='ch.ehi.ili2db.c1Min' THEN 1"
+                stmt += ln + "            WHEN cprop.tag='ch.ehi.ili2db.c2Min' THEN 2"
+                stmt += ln + "            WHEN cprop.tag='ch.ehi.ili2db.c1Max' THEN 3"
+                stmt += ln + "            WHEN cprop.tag='ch.ehi.ili2db.c2Max' THEN 4 END)"
+                stmt += ln + "        FROM \"{schema}\".T_ILI2DB_COLUMN_PROP cprop"
+                stmt += ln + "        WHERE tbls.TABLE_NAME = UPPER(cprop.TABLENAME) AND clm.COLUMN_NAME = UPPER(cprop.COLUMNNAME)"
+                stmt += ln + "            AND cprop.tag IN ('ch.ehi.ili2db.c1Min', 'ch.ehi.ili2db.c2Min', 'ch.ehi.ili2db.c1Max', 'ch.ehi.ili2db.c2Max')"
+                stmt += ln + "        ) AS extent"
+                stmt += ln + "    , tgeomtype.SETTING as simple_type"
+                stmt += ln + "    , NULL AS formatted_type"
+            stmt += ln + "FROM ALL_CONSTRAINTS Tab"
+            stmt += ln + "INNER JOIN ALL_CONS_COLUMNS Col"
+            stmt += ln + "    ON Col.CONSTRAINT_NAME = Tab.CONSTRAINT_NAME"
+            stmt += ln + "    AND Col.TABLE_NAME = Tab.TABLE_NAME"
+            stmt += ln + "    AND Tab.OWNER = Col.OWNER"
+            stmt += ln + "    AND Tab.CONSTRAINT_TYPE= 'P'"
+            stmt += ln + "RIGHT JOIN ALL_TABLES tbls"
+            stmt += ln + "    ON Tab.TABLE_NAME = tbls.TABLE_NAME "
+            stmt += ln + "    AND Tab.OWNER = tbls.OWNER"
+            stmt += ln + "LEFT JOIN ALL_TAB_COLUMNS clm"
+            stmt += ln + "    ON clm.TABLE_NAME = tbls.TABLE_NAME"
+            stmt += ln + "    AND clm.OWNER = tbls.OWNER"
+            stmt += ln + "    AND clm.DATA_TYPE = 'SDO_GEOMETRY'"
+            if metadata_exists:
+                stmt += ln + "LEFT JOIN \"{schema}\".T_ILI2DB_TABLE_PROP p"
+                stmt += ln + "    ON UPPER(p.TABLENAME) = tbls.TABLE_NAME "
+                stmt += ln + "    AND p.TAG = 'ch.ehi.ili2db.tableKind' "
+                stmt += ln + "LEFT JOIN \"{schema}\".T_ILI2DB_TABLE_PROP alias"
+                stmt += ln + "    ON UPPER(alias.TABLENAME) = tbls.TABLE_NAME"
+                stmt += ln + "    AND alias.TAG = 'ch.ehi.ili2db.dispName'"
+                stmt += ln + "LEFT JOIN \"{schema}\".T_ILI2DB_CLASSNAME c"
+                stmt += ln + "    ON tbls.TABLE_NAME = UPPER(c.SQLNAME)"
+                stmt += ln + "LEFT JOIN \"{schema}\".T_ILI2DB_COLUMN_PROP tsrid"
+                stmt += ln + "    ON tbls.TABLE_NAME = UPPER(tsrid.TABLENAME) "
+                stmt += ln + "    AND clm.COLUMN_NAME = UPPER(tsrid.COLUMNNAME)"
+                stmt += ln + "    AND tsrid.TAG='ch.ehi.ili2db.srid'"
+                stmt += ln + "LEFT JOIN \"{schema}\".T_ILI2DB_COLUMN_PROP tgeomtype"
+                stmt += ln + "    ON tbls.TABLE_NAME = UPPER(tgeomtype.TABLENAME) "
+                stmt += ln + "    AND clm.COLUMN_NAME = UPPER(tgeomtype.COLUMNNAME)"
+                stmt += ln + "    AND tgeomtype.TAG='ch.ehi.ili2db.geomType'"
+            stmt += ln + "WHERE tbls.OWNER = '{schema}'"
+            stmt = stmt.format(schema=self.schema)
+
+            query = QSqlQuery(self.conn)
+
+            if not query.exec(stmt):
+                error = query.lastError().text()
+                raise DBConnectorError(error)
+
+            res = self._get_dict_result(query)
+            for row in res:
+                row['srid'] = int(row['srid']) if row['srid'] else None
+                row['type'] = row['simple_type']
+        return res
+
     def _table_exists(self, tablename):
         result = False
 
@@ -122,6 +197,6 @@ class OraConnector(DBConnector):
         while query.next():
             my_rec = dict()
             for x in range(record.count()):
-                my_rec[record.fieldName(x)] = query.value(x)
+                my_rec[record.fieldName(x).lower()] = query.value(x)
             result.append(my_rec)
         return result
